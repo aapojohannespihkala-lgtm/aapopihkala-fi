8e7d9f17b2d7c5033cfed80a61ad2bbb0cde4d17
aapojohannespihkala-lgtm <aapo.johannes.pihkala@gmail.com>
2026-08-31 14:39:07 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://aapopihkala.fi'
+  site: 'https://aapopihkala.fi',
+  integrations: [sitemap()]
 });
