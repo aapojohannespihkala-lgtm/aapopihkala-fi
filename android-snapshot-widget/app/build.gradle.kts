0a12a2728454b056aba10e6392e75a22836d2d7f
aapojohannespihkala-lgtm <aapo.johannes.pihkala@gmail.com>
2026-09-14 09:11:23 +0300

    Remove obsolete header time code (#496)
    * Remove dead header time code
    
    * Bump widget to 2.9.2
    
    * temporary placeholder
    
    * Remove temporary placeholder

--- android-snapshot-widget/app/build.gradle.kts
+++ android-snapshot-widget/app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "fi.aapopihkala.snapshotwidget"
         minSdk = 23
         targetSdk = 36
-        versionCode = 22
-        versionName = "2.9.1"
+        versionCode = 23
+        versionName = "2.9.2"
     }
 
     if (signingStorePath != null && signingStorePassword != null) {
