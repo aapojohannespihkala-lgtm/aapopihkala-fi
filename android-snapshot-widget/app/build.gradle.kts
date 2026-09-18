279e7fd7751c2209458e86b8a16f2dc5e69259ce
aapojohannespihkala-lgtm <aapo.johannes.pihkala@gmail.com>
2026-09-18 15:57:42 +0300

    Bump Snapshot Widget to 2.10.54

--- android-snapshot-widget/app/build.gradle.kts
+++ android-snapshot-widget/app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "fi.aapopihkala.snapshotwidget"
         minSdk = 23
         targetSdk = 36
-        versionCode = 82
-        versionName = "2.10.53"
+        versionCode = 83
+        versionName = "2.10.54"
     }
 
     if (signingStorePath != null && signingStorePassword != null) {
